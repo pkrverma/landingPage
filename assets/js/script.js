dcee459ef5f87688d9a57c203524f6ff5c23804e
Pulkit Verma <pulkit18012003@gmail.com>
2024-01-23 00:12:30 +0530

    First hand all complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// ==============CountDown timer==============
 var countDownDate = new Date("Jan 31, 2024 12:00:00").getTime();
 
 var x = setInterval(function () {
@@ -34,6 +35,8 @@ function onVisible(element, callback) {
     if (!callback) return new Promise(r => callback = r);
 }
 
+// ==============Stats Counter==============
+
 onVisible(document.querySelector("#event"), () => {
     function counter(id, start, end, duration) {
         let obj = document.getElementById(id),
@@ -54,3 +57,87 @@ onVisible(document.querySelector("#event"), () => {
     counter("count3", 0, 8, 2000);
     counter("count4", 0, 5, 2000);
 });
+
+// ==============Comming up==============
+
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
+};
+
+const texts = [
+    "Hackathon 3.0",
+    "Organised By",
+    "Tech Community",
+    "     "
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+
+    let fraction = morph / morphTime;
+
+    if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+
+    setMorph(fraction);
+}
+
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+
+    cooldown -= dt;
+
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+            textIndex++;
+        }
+
+        doMorph();
+    } else {
+        doCooldown();
+    }
+}
+
+animate();
